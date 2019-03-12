bf7badea60a628f2cd75e09ce06a84b545b91b24
Wejdi <wejdigharbi@yahoo.fr>
2019-03-12 23:58:05 +0100

    Updates

---
+++ precache-manifest.d4695aaf90de0f7a5c4229935b3207fe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "120076c7e404fb282e53f6885f38dac7",
+    "url": "/Docplanner-Clone-with-React/static/media/doctors.120076c7.png"
+  },
+  {
+    "revision": "f26719a71398a3f2e53c",
+    "url": "/Docplanner-Clone-with-React/static/css/main.d3cab4cc.chunk.css"
+  },
+  {
+    "revision": "457b3408d72f9fd7a37c",
+    "url": "/Docplanner-Clone-with-React/static/js/1.457b3408.chunk.js"
+  },
+  {
+    "revision": "012dff83a020b1a9bf9c",
+    "url": "/Docplanner-Clone-with-React/static/js/runtime~main.012dff83.js"
+  },
+  {
+    "revision": "4617f89b0fe16a5ff42dc353495dbdd4",
+    "url": "/Docplanner-Clone-with-React/static/media/rome.4617f89b.png"
+  },
+  {
+    "revision": "82d323de468a8ad9636c43e8de2a26ba",
+    "url": "/Docplanner-Clone-with-React/static/media/patients.82d323de.png"
+  },
+  {
+    "revision": "f26719a71398a3f2e53c",
+    "url": "/Docplanner-Clone-with-React/static/js/main.f26719a7.chunk.js"
+  },
+  {
+    "revision": "9d829d85c292405acbb619a69aa1c7fa",
+    "url": "/Docplanner-Clone-with-React/static/media/warsaw.9d829d85.png"
+  },
+  {
+    "revision": "152f007cb7e202b072209d1c95eb076b",
+    "url": "/Docplanner-Clone-with-React/static/media/barcelona.152f007c.png"
+  },
+  {
+    "revision": "40d3772597883d2e2b04e59087633ba2",
+    "url": "/Docplanner-Clone-with-React/static/media/istanbul.40d37725.png"
+  },
+  {
+    "revision": "47ca4c93d3fb1d4d6f655284da529ef6",
+    "url": "/Docplanner-Clone-with-React/static/media/mexico.47ca4c93.png"
+  },
+  {
+    "revision": "0118191586ed11a49419a3cda8f82439",
+    "url": "/Docplanner-Clone-with-React/index.html"
+  }
+];
